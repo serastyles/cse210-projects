75a2bc324e208990d9e56d6c16c4fd7af510dea3
serasyles <snakanwagi@byupathway.edu>
2025-07-07 17:43:35 +0300

    exchanged lines

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?.");
-        Console.WriteLine("What is your last name?");
+        Console.WriteLine("What is your first name?");
         string firstname = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
         String lastname = Console.ReadLine();
         Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
