77874c7319112a7ebe413d530effabeeaac62ea9
serasyles <snakanwagi@byupathway.edu>
2025-07-07 18:37:41 +0300

    Updated names

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("What is your first name?");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.WriteLine("What is your last name? ");
-        String lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+        String lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
