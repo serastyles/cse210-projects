20cc4df226ba1d67edc002672d842edabfaa414d
serasyles <snakanwagi@byupathway.edu>
2025-07-07 17:31:05 +0300

    wrote a code that asks the user for their first and last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name?.");
+        Console.WriteLine("What is your last name?");
+        string firstname = Console.ReadLine();
+        String lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
