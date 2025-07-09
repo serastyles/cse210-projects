300d401ff0b6a03d09966a016fb537629bc0732d
serasyles <snakanwagi@byupathway.edu>
2025-07-09 22:42:40 +0300

    added modifications to the code

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,57 @@
 using System;
-
-class Program
+using System.Collections.Generic;
+    public class Job
 {
-    static void Main(string[] args)
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.WriteLine($"{_jobTitle} ({_company}){_startYear}-{_endYear}");
+
     }
+    public class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs:");
+            foreach (Job job in _jobs)
+            {
+                job.Display();
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Job job1 = new Job();
+            job1._jobTitle = "Software Developer";
+            job1._company = "Microsoft";
+            job1._startYear = 2019;
+            job1._endYear = 2022;
+
+            Job job2 = new Job();
+            job2._jobTitle = "Manager";
+            job2._company = "Apple";
+            job2._startYear = 2022;
+            job2._endYear = 2023;
+
+            Resume myResume = new Resume();
+            myResume._name = "Sarah Nakanwagi";
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
+
+            myResume.Display();
+        }
+    
+    }
+   
 }
