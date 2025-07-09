40398bf784f84975c4767eb6d79094c275586f02
serasyles <snakanwagi@byupathway.edu>
2025-07-09 11:58:42 +0300

    added a random generator

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        // Ask for the user's guess
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        // Compare the guess with the magic number
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Lower, try a higher one.");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Oh, this is Higher.");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
