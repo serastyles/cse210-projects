c95e8b48d2415b93165e4e46a3b71eecea466f94
serasyles <snakanwagi@byupathway.edu>
2025-07-28 22:03:31 +0300

    added a screen shot and saved changes to program files

---
+++ week04/OnlineOrdering/product.cs
@@ -0,0 +1,25 @@
+public class Product
+{
+    private string _name;
+    private string _productId;
+    private decimal _pricePerUnit;
+    private int _quantity;
+
+    public Product(string name, string productId, decimal pricePerUnit, int quantity)
+    {
+        _name = name;
+        _productId = productId;
+        _pricePerUnit = pricePerUnit;
+        _quantity = quantity;
+    }
+
+    public decimal GetTotalCost()
+    {
+        return _pricePerUnit * _quantity;
+    }
+
+    public string GetPackingLabel()
+    {
+        return $"{_name} (ID: {_productId})";
+    }
+}
