de45599c22d8fee77e31046266face271d200b1e
serasyles <snakanwagi@byupathway.edu>
2025-08-01 18:24:05 +0300

    modified the interval before the next breathe in and a comment to the breathing activity class

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,5 +1,5 @@
 // Added a meditative pause between cycles 
-
+// Added a 2 seconds interval for one to pull enough air before the next breath in.
 public class BreathingActivity : Activity
 {
     public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.") { }
@@ -10,13 +10,15 @@ public class BreathingActivity : Activity
         int elapsed = 0;
         while (elapsed < _duration)
         {
+            Console.Write("...");
+            Thread.Sleep(2000);
             Console.WriteLine("\nBreathe in...");
             Countdown(6);
             elapsed += 4;
 
             if (elapsed >= _duration) break;
             Console.WriteLine("Breathe out...");
-            Countdown(5);
+            Countdown(2);
             elapsed += 4;
 
             }
